4db4c2bcd15c2dfab3039d47d837555e4ef59961
Nik.Brosko <Nikita.brosko@mail.ru>
2021-11-21 13:57:52 +0300

    Add abstract CandyBase and ChocolateBar

---
+++ NewYearPresent/NewYearPresent/Sweets/Candies/CandyBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NewYearPresent.Sweets.Candies
+{
+    abstract class CandyBase : ISweetness
+    {
+        public string Name { get; protected set; }
+        public decimal Price { get; protected set; }
+        public float Weight { get; protected set; }
+        public float AmountOfSugar { get; private set; }
+
+        public virtual void CalculateAmountOfSugar()
+        {
+            CheckWeight();
+
+            AmountOfSugar = Weight / 2;
+        }
+
+        protected void CheckWeight()
+        {
+            if (Weight == 0)
+            {
+                throw new ArgumentException("Property Weight can not be 0!");
+            }
+        }
+    }
+}
---
+++ NewYearPresent/NewYearPresent/Sweets/ChocolateBars/ChocolateBar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NewYearPresent.Sweets.ChocolateBars
+{
+    public abstract class ChocolateBar : ISweetness
+    {
+        public string Name { get; protected set; }
+        public decimal Price { get; protected set; }
+        public float Weight { get; protected set; }
+        public float AmountOfSugar { get; private set; }
+
+        public void CalculateAmountOfSugar()
+        {
+            if (Weight == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            AmountOfSugar = (Weight / 2) + (Weight % 2);
+        }
+    }
+}
