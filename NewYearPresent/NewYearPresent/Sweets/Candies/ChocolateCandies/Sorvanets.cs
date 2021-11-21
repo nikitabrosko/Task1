9fe39040a4af927fe78a51b2914d32df3ff9f9c9
Nik.Brosko <Nikita.brosko@mail.ru>
2021-11-21 13:59:01 +0300

    Add ChocolateCandy abstract class and one Chocolate candy

---
+++ NewYearPresent/NewYearPresent/Sweets/Candies/ChocolateCandies/Sorvanets.cs
@@ -0,0 +1,14 @@
+﻿namespace NewYearPresent.Sweets.Candies.ChocolateCandies
+{
+    sealed class Sorvanets : ChocolateCandy
+    {
+        public Sorvanets()
+        {
+            Name = "Sorvanets";
+            Price = 0.5m;
+            Weight = 8.9f;
+
+            CalculateAmountOfSugar();
+        }
+    }
+}
