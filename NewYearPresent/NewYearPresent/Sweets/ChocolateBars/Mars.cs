c495317418c8a7badf2e528f7b262b46ead845f3
Nik.Brosko <Nikita.brosko@mail.ru>
2021-11-21 13:59:41 +0300

    Add 1 ChocolateBar

---
+++ NewYearPresent/NewYearPresent/Sweets/ChocolateBars/Mars.cs
@@ -0,0 +1,14 @@
+﻿namespace NewYearPresent.Sweets.ChocolateBars
+{
+    class Mars : ChocolateBar
+    {
+        public Mars()
+        {
+            Name = "Mars";
+            Price = 0.3m;
+            Weight = 7.5f;
+
+            CalculateAmountOfSugar();
+        }
+    }
+}
