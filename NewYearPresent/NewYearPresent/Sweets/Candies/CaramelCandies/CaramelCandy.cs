628f1571cd39ffda30715ec33b97f0726609eddf
Nik.Brosko <Nikita.brosko@mail.ru>
2021-11-21 14:13:13 +0300

    Add abstract class CaramelCandy and 1 caramel candy

---
+++ NewYearPresent/NewYearPresent/Sweets/Candies/CaramelCandies/CaramelCandy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewYearPresent.Sweets.Candies.CaramelCandies
+{
+    abstract class CaramelCandy : CandyBase
+    {
+        public new float AmountOfSugar { get; private set; }
+
+        public override void CalculateAmountOfSugar()
+        {
+            CheckWeight();
+
+            AmountOfSugar = (Weight / 2.5f) + (Weight % 2);
+        }
+    }
+}
---
+++ NewYearPresent/NewYearPresent/Sweets/Candies/CaramelCandies/Malibu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewYearPresent.Sweets.Candies.CaramelCandies
+{
+    sealed class Malibu : CaramelCandy
+    {
+        public Malibu()
+        {
+            Name = "Malibu";
+            Price = 0.6m;
+            Weight = 5.4f;
+
+            CalculateAmountOfSugar();
+        }
+    }
+}
