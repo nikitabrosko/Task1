8b21195aa812742f8a406cfd727f0ceafdf7241b
Nik.Brosko <Nikita.brosko@mail.ru>
2021-11-24 16:17:25 +0300

    Create an UnitTests project

---
+++ NewYearPresent/NewYearPresentTests/SweetsTests/ChocolateBarsTests/ChocolateBarTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NewYearPresentTests.SweetsTests.ChocolateBarsTests
+{
+    [TestClass]
+    public class ChocolateBarTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
