d469963debf9d85adc7c7c336dca9b8b6cfc08ab
Nik.Brosko <Nikita.brosko@mail.ru>
2021-11-25 19:43:37 +0300

    Create class SweetnessBase

---
+++ NewYearPresent/NewYearPresent/Sweets/SweetnessBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace NewYearPresent.Sweets
+{
+    public abstract class SweetnessBase : ISweetness
+    {
+        private string _name;
+        private decimal _price;
+        private float _weight;
+
+        public string Name
+        {
+            get => _name;
+            protected set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(nameof(Name));
+                }
+
+                _name = value;
+            }
+        }
+        public decimal Price
+        {
+            get => _price;
+            protected set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(nameof(Price));
+                }
+
+                _price = value;
+            }
+        }
+
+        public float Weight
+        {
+            get => _weight;
+            protected set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(nameof(Weight));
+                }
+
+                _weight = value;
+            }
+        }
+
+        public abstract float AmountOfSugar { get; }
+    }
+}
