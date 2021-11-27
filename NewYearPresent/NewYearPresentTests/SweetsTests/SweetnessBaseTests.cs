a6015e5f64ee67ac0691e0f5cc42562ff036b9c9
Nik.Brosko <Nikita.brosko@mail.ru>
2021-11-27 19:01:14 +0300

    Install Moq to Tests project and create a Test class

---
+++ NewYearPresent/NewYearPresentTests/SweetsTests/SweetnessBaseTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NewYearPresent.Sweets;
+using NewYearPresent.Sweets.GummyCandies;
+using NewYearPresentTests.SweetsTests.GummyCandiesTests;
+
+namespace NewYearPresentTests.SweetsTests
+{
+    [TestClass]
+    public class SweetnessBaseTests
+    {
+        [TestMethod]
+        public void SetInvalidName()
+        {
+            var mock = new Mock<GummyCandy>();
+            mock.Setup(x => new TestGummyCandy("Test", 1, 10));
+
+            var amountOfSugar = mock.Object.AmountOfSugar;
+
+            Assert.AreEqual(3.3f, amountOfSugar);
+        }
+    }
+}
