66199e6da733e6e5f4343458a6edba9b094e4efd
Nik.Brosko <Nikita.brosko@mail.ru>
2021-11-27 17:57:27 +0300

    Add a new implementation and refactor old

--- NewYearPresent/NewYearPresent/Builders/CandyContainerBuilder/Maker.cs
+++ NewYearPresent/NewYearPresent/Builders/CandyContainerBuilder/Maker.cs
@@ -11,19 +11,74 @@ namespace NewYearPresent.Builders.CandyContainerBuilder
 {
     public class Maker
     {
-        private static IList<ISweetness> DefaultSweets =>
-            new List<ISweetness>()
+        private static IList<CaramelCandy> CaramelCandies =>
+            new List<CaramelCandy>()
+            {
+                new MalibuCaramelCandy(),
+                new AlenkaCaramelCandy(),
+                new LollipopCaramelCandy(),
+                new MolochnayaCaramelCandy(),
+                new ZubastikiCaramelCandy()
+            };
+        private static IList<ChocolateBar> ChocolateBars =>
+            new List<ChocolateBar>()
             {
                 new MarsChocolateBar(),
-                new SorvanetsChocolateCandy(),
-                new JellyGummyCandy(),
-                new MalibuCaramelCandy()
+                new BountyChocolateBar(),
+                new KitKatChocolateBar(),
+                new LionChocolateBar(),
+                new SnickersChocolateBar()
             };
+        private static IList<ChocolateCandy> ChocolateCandies =>
+            new List<ChocolateCandy>()
+            {
+                new SorvanetsChocolateCandy(),
+                new AlenkaChocolateCandy(),
+                new KommunarkaChocolateCandy(),
+                new KuznechikChocolateCandy(),
+                new MinchankaChocolateCandy()
+            };
+        private static IList<GummyCandy> GummyCandies =>
+            new List<GummyCandy>()
+            {
+                new JellyGummyCandy(),
+                new CrazyBeeGummyCandy(),
+                new GummyBearGummyCandy(),
+                new HariboGummyCandy(),
+                new MatreshkaGummyCandy()
+            };
+
+        private static IList<ISweetness> GetAllSweets()
+        {
+            IList<ISweetness> sweets = new List<ISweetness>();
+
+            foreach (var caramelCandy in CaramelCandies)
+            {
+                sweets.Add(caramelCandy);
+            }
+
+            foreach (var chocolateBar in ChocolateBars)
+            {
+                sweets.Add(chocolateBar);
+            }
+
+            foreach (var chocolateCandy in ChocolateCandies)
+            {
+                sweets.Add(chocolateCandy);
+            }
+
+            foreach (var gummyCandy in GummyCandies)
+            {
+                sweets.Add(gummyCandy);
+            }
+
+            return sweets;
+        }
 
         public static ICandyContainer MakeDefaultCandyContainer(CandyContainerBuilder candyContainerBuilder)
         {
             candyContainerBuilder.CreateCandyContainer();
-            candyContainerBuilder.AddSweets(DefaultSweets);
+            candyContainerBuilder.AddSweets(GetAllSweets());
             return candyContainerBuilder.GetResult();
         }
 
@@ -31,11 +86,13 @@ namespace NewYearPresent.Builders.CandyContainerBuilder
         {
             var random = new Random();
 
+            var sweets = GetAllSweets();
+
             candyContainerBuilder.CreateCandyContainer();
 
             for (int i = 0; i < countOfCandies; i++)
             {
-                candyContainerBuilder.AddSweetness(DefaultSweets[random.Next(0, DefaultSweets.Count - 1)]);
+                candyContainerBuilder.AddSweetness(sweets[random.Next(0, sweets.Count - 1)]);
             }
 
             return candyContainerBuilder.GetResult();
@@ -43,40 +100,32 @@ namespace NewYearPresent.Builders.CandyContainerBuilder
 
         public static ICandyContainer MakeChocolateCandyContainer(CandyContainerBuilder candyContainerBuilder)
         {
-            IList<ISweetness> sweets = new List<ISweetness>()
-            {
-                new SorvanetsChocolateCandy(),
-                new MarsChocolateBar()
-            };
-
             candyContainerBuilder.CreateCandyContainer();
-            candyContainerBuilder.AddSweets(sweets);
+            candyContainerBuilder.AddSweets(ChocolateCandies);
 
             return candyContainerBuilder.GetResult();
         }
 
         public static ICandyContainer MakeCaramelCandyContainer(CandyContainerBuilder candyContainerBuilder)
         {
-            IList<ISweetness> sweets = new List<ISweetness>()
-            {
-                new MalibuCaramelCandy()
-            };
-
             candyContainerBuilder.CreateCandyContainer();
-            candyContainerBuilder.AddSweets(sweets);
+            candyContainerBuilder.AddSweets(CaramelCandies);
 
             return candyContainerBuilder.GetResult();
         }
 
         public static ICandyContainer MakeGummyCandyContainer(CandyContainerBuilder candyContainerBuilder)
         {
-            IList<ISweetness> sweets = new List<ISweetness>()
-            {
-                new JellyGummyCandy()
-            };
-
             candyContainerBuilder.CreateCandyContainer();
-            candyContainerBuilder.AddSweets(sweets);
+            candyContainerBuilder.AddSweets(GummyCandies);
+
+            return candyContainerBuilder.GetResult();
+        }
+
+        public static ICandyContainer MakeChocolateBarContainer(CandyContainerBuilder candyContainerBuilder)
+        {
+            candyContainerBuilder.CreateCandyContainer();
+            candyContainerBuilder.AddSweets(ChocolateBars);
 
             return candyContainerBuilder.GetResult();
         }
