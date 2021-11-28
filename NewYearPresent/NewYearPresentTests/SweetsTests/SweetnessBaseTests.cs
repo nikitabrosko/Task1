31a8efada0d3d6069cf0497457de1b6c41cb3b82
Nik.Brosko <Nikita.brosko@mail.ru>
2021-11-28 15:34:06 +0300

    Create a 1 instance of sweetness and 1 class for testing all sweets classes

--- NewYearPresent/NewYearPresentTests/SweetsTests/SweetnessBaseTests.cs
+++ NewYearPresent/NewYearPresentTests/SweetsTests/SweetnessBaseTests.cs
@@ -1,28 +1,116 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
 using NewYearPresent.Sweets;
+using NewYearPresent.Sweets.CaramelCandies;
+using NewYearPresent.Sweets.ChocolateBars;
+using NewYearPresent.Sweets.ChocolateCandies;
 using NewYearPresent.Sweets.GummyCandies;
-using NewYearPresentTests.SweetsTests.GummyCandiesTests;
 
 namespace NewYearPresentTests.SweetsTests
 {
     [TestClass]
     public class SweetnessBaseTests
     {
+        //Testing Sweets classes AmountOfSugar property
         [TestMethod]
-        public void SetInvalidName()
+        [DataRow(typeof(MalibuCaramelCandy), 3.6f)]
+        public void CheckReturningAmountOfSugarForCaramelCandies(Type candyType, float expectedAmountOfSugar)
         {
-            var mock = new Mock<GummyCandy>();
-            mock.Setup(x => new TestGummyCandy("Test", 1, 10));
+            var candyInstance = (ISweetness)Activator.CreateInstance(candyType);
 
-            var amountOfSugar = mock.Object.AmountOfSugar;
+            Assert.AreEqual(expectedAmountOfSugar, candyInstance.AmountOfSugar);
+        }
 
-            Assert.AreEqual(3.3f, amountOfSugar);
+        [TestMethod]
+        [DataRow(typeof(MarsChocolateBar), 23.9f)]
+        public void CheckReturningAmountOfSugarForChocolateBars(Type candyType, float expectedAmountOfSugar)
+        {
+            var candyInstance = (ISweetness)Activator.CreateInstance(candyType);
+
+            Assert.AreEqual(expectedAmountOfSugar, candyInstance.AmountOfSugar);
+        }
+
+        [TestMethod]
+        [DataRow(typeof(AlenkaChocolateCandy), 5.4f)]
+        public void CheckReturningAmountOfSugarForChocolateCandies(Type candyType, float expectedAmountOfSugar)
+        {
+            var candyInstance = (ISweetness) Activator.CreateInstance(candyType);
+
+            Assert.AreEqual(expectedAmountOfSugar, candyInstance.AmountOfSugar);
+        }
+
+        [TestMethod]
+        [DataRow(typeof(JellyGummyCandy), 2.4f)]
+        public void CheckReturningAmountOfSugarForGummyCandies(Type candyType, float expectedAmountOfSugar)
+        {
+            var candyInstance = (ISweetness)Activator.CreateInstance(candyType);
+
+            Assert.AreEqual(expectedAmountOfSugar, candyInstance.AmountOfSugar);
+        }
+
+        //Testing Sweets classes Name, Price and Weight properties
+        [TestMethod]
+        [DataRow("", 1, 1)]
+        [DataRow(null, 1, 1)]
+        [DataRow(" ", 1, 1)]
+        public void SetInvalidName(string name, double price, float weight)
+        {
+            Assert.ThrowsException<ArgumentException>(() => new TestSweetness(name, (decimal)price, weight),
+                nameof(TestSweetness.Name));
+        }
+
+        [TestMethod]
+        [DataRow("Test", 1, 1)]
+        [DataRow("Name", 1, 1)]
+        public void SetValidName(string name, double price, float weight)
+        {
+            TestSweetness testChocolateBar = new TestSweetness(name, (decimal)price, weight);
+
+            Assert.AreEqual(name, testChocolateBar.Name);
+        }
+
+        [TestMethod]
+        [DataRow("Test", 0, 1)]
+        [DataRow("Test", -1, 1)]
+        [DataRow("Test", -10, 1)]
+        [DataRow("Test", -100, 1)]
+        public void SetInvalidPrice(string name, double price, float weight)
+        {
+            Assert.ThrowsException<ArgumentException>(() => new TestSweetness(name, (decimal)price, weight),
+                nameof(TestSweetness.Price));
+        }
+
+        [TestMethod]
+        [DataRow("Test", 1, 1)]
+        [DataRow("Test", 10, 1)]
+        [DataRow("Test", 100, 1)]
+        public void SetValidPrice(string name, double price, float weight)
+        {
+            TestSweetness testChocolateBar = new TestSweetness(name, (decimal)price, weight);
+
+            Assert.AreEqual((decimal)price, testChocolateBar.Price);
+        }
+
+        [TestMethod]
+        [DataRow("Test", 1, 0)]
+        [DataRow("Test", 1, -1)]
+        [DataRow("Test", 1, -10)]
+        [DataRow("Test", 1, -100)]
+        public void SetInvalidWeight(string name, double price, float weight)
+        {
+            Assert.ThrowsException<ArgumentException>(() => new TestSweetness(name, (decimal)price, weight),
+                nameof(TestSweetness.Weight));
+        }
+
+        [TestMethod]
+        [DataRow("Test", 1, 1)]
+        [DataRow("Test", 1, 10)]
+        [DataRow("Test", 1, 100)]
+        public void SetValidWeight(string name, double price, float weight)
+        {
+            TestSweetness testChocolateBar = new TestSweetness(name, (decimal)price, weight);
+
+            Assert.AreEqual(weight, testChocolateBar.Weight);
         }
     }
 }
---
+++ NewYearPresent/NewYearPresentTests/SweetsTests/TestSweetness.cs
@@ -0,0 +1,14 @@
+﻿using NewYearPresent.Sweets.ChocolateCandies;
+
+namespace NewYearPresentTests.SweetsTests
+{
+    public sealed class TestSweetness : ChocolateCandy
+    {
+        public TestSweetness(string name, decimal price, float weight)
+        {
+            Name = name;
+            Price = price;
+            Weight = weight;
+        }
+    }
+}
